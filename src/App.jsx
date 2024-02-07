d682e9bf2f9b359605bbbff1b995737a8f29c379
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-02-07 18:24:21 -0300

    feat: shows who won the game

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function getCurrentPlayer(gameTurns) {
   let currentPlayer = "X";
@@ -17,6 +24,34 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = getCurrentPlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, cell } = square;
+
+    gameBoard[row][cell] = player;
+  }
+
+  let winner = null;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handleSelectPlayer(rowIndex, cellIndex) {
     setGameTurns((prevGameTurns) => {
@@ -48,8 +83,9 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
+        {winner && <p>{winner} wins!</p>}
+        <GameBoard onSelectPlayer={handleSelectPlayer} board={gameBoard} />
       </div>
-      <GameBoard onSelectPlayer={handleSelectPlayer} turns={gameTurns} />
       <Log turns={gameTurns} />
     </main>
   );
